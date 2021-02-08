e3f93859d5faa77be98ca7c2daf323d11b21d01b
New-Yerk <yi2027@nyu.edu>
2021-02-08 18:40:05 +0400

    thingsss

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ let CloseButton = document.getElementById('Closing_Button')
 let ConceptButton = document.getElementById('Concept')
 let NewImage = document.getElementById('Campus')
 let PopUp = document.getElementById('PopUp_Us')
+let PopUpIlya = document.getElementById('PopUp_Ilya')
+let Return = document.getElementById('CampusButton')
+let Return2 = document. getElementById('Campus')
 PopUp.style.display = 'none';
+PopUpIlya.style.display = 'none';
 PerformersButton.addEventListener ('click', showPopUp);
 function showPopUp(){
   PopUp.style.display='block';
+  PopUpIlya.style.display = 'block';
 }
 CloseButton.addEventListener ('click', hidePopUp);
 function hidePopUp(){
   PopUp.style.display = 'none';
+  PopUpIlya.style.display = 'none';
 }
 ConceptButton.addEventListener ('click', addImg);
 function addImg () {
   NewImage.src = "media/funkytown.jpg";
+}
+Return.addEventListener('click', addAnotherImg)
+function addAnotherImg () {
+  Return2.src = "media/campus.jpg";
 }
